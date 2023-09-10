c3d45f70f97ae0ecdfbc716131abc98254a63c48
natmacira <natimciraolo@gmail.com>
2023-09-10 20:37:05 +0200

    started the publications html

---
+++ page-publications.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Empty Base
+ */
+
+/**
+ * It uses the same layout that the home page.
+ */
+
+?>
+<section class="publications-main-section">
+    <article class="titles">
+        <h1>Cutre Lux Jantus</h1>
+        <h2>PUBLICATIONS</h2>
+    </article>
+    <article class="library">
+        <div class="library-inner-element">
+            <div>
+                <h3>LA RADIO BARCELONA</h3>
+                <span>full interview</span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p>BARCELONA. <br> 2018</p>
+        </div>
+        <div >
+            <div>
+                <h3></h3>
+                <span></span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p></p>
+        </div> <div >
+            <div>
+                <h3></h3>
+                <span></span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p></p>
+        </div> <div >
+            <div>
+                <h3></h3>
+                <span></span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p></p>
+        </div> <div >
+            <div>
+                <h3></h3>
+                <span></span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p></p>
+        </div> <div >
+            <div>
+                <h3></h3>
+                <span></span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p></p>
+        </div> <div >
+            <div>
+                <h3></h3>
+                <span></span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p></p>
+        </div> <div >
+            <div>
+                <h3></h3>
+                <span></span>
+            </div>
+            <iframe src="" frameborder="0"></iframe>
+            <p></p>
+        </div>
+    </article>
+</section>
