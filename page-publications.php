2d7a3cc89f0927fa78390d6b87680365e7b3c0f9
natmacira <natimciraolo@gmail.com>
2023-09-11 19:55:41 +0200

    done logic for open books

--- page-publications.php
+++ page-publications.php
@@ -11,91 +11,118 @@ require_once 'header.php';
 </section>
 <section class="library">
     <article class="library-inner-element">
-        <div class="book-content">
-            <h3>LA RADIO BARCELONA</h3>
+        <div class="one-book-content">
+            <h3>BONART</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
-            <p>BARCELONA. <br> 2018</p>
+            <p>test<br> 2018</p>
         </div>
-        <div class="book-title">
-            <p><span>18</span> LA RADIO BARCELONA</p>
+        <div class="one-book-title">
+            <p><span>18</span> BONART</p>
         </div>
     </article>
+
+
     <article class="library-inner-element">
-        <div class="book-content">
+        <div class="two-book-content">
+            <h3>contemporary art curator</h3>
+            <span>full interview</span>
+            <iframe src="" frameborder="0"></iframe>
+            <p>test<br> 2018</p>
+        </div>
+        <div class="two-book-title">
+             <p><span>18</span> contemporary art curator</p>
+        </div>
+    </article>
+
+
+    <article class="library-inner-element">
+        <div class="three-book-content">
             <h3>LA RADIO BARCELONA</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
             <p>BARCELONA. <br> 2018</p>
         </div>
-        <div class="book-title">
+        <div class="three-book-title">
              <p><span>18</span> LA RADIO BARCELONA</p>
         </div>
     </article>
+
+
     <article class="library-inner-element">
-        <div class="book-content">
+        <div class="four-book-content">
             <h3>LA RADIO BARCELONA</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
             <p>BARCELONA. <br> 2018</p>
         </div>
-        <div class="book-title">
-             <p><span>18</span> LA RADIO BARCELONA</p>
+        <div class="four-book-title">
+             <p><span>18</span>artsy</p>
         </div>
     </article>
+
+
     <article class="library-inner-element">
-        <div class="book-content">
+        <div class="five-book-content">
             <h3>LA RADIO BARCELONA</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
             <p>BARCELONA. <br> 2018</p>
         </div>
-        <div class="book-title">
-             <p><span>18</span> LA RADIO BARCELONA</p>
+        <div class="five-book-title">
+             <p><span>18</span> ARTQOL</p>
         </div>
     </article>
+
+
     <article class="library-inner-element">
-        <div class="book-content">
+        <div class="six-book-content">
             <h3>LA RADIO BARCELONA</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
             <p>BARCELONA. <br> 2018</p>
         </div>
-        <div class="book-title">
-             <p><span>18</span> LA RADIO BARCELONA</p>
+        <div class="six-book-title">
+             <p><span>18</span>Kirjasta-kirjaan</p>
         </div>
     </article>
+
+
     <article class="library-inner-element">
-        <div class="book-content">
+        <div class="seven-book-content">
             <h3>LA RADIO BARCELONA</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
             <p>BARCELONA. <br> 2018</p>
         </div>
-        <div class="book-title">
-             <p><span>18</span> LA RADIO BARCELONA</p>
+        <div class="seven-book-title">
+             <p><span>18</span>diffuse reality </p>
         </div>
     </article>
+
+
     <article class="library-inner-element">
-        <div class="book-content">
+        <div class="eight-book-content">
             <h3>LA RADIO BARCELONA</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
             <p>BARCELONA. <br> 2018</p>
         </div>
-        <div class="book-title">
-             <p><span>18</span> LA RADIO BARCELONA</p>
+        <div class="eight-book-title">
+             <p><span>18</span>chasama </p>
         </div>
     </article>
+   
+   
     <article class="library-inner-element">
-        <div class="book-content">
+        <div class="nine-book-content">
             <h3>LA RADIO BARCELONA</h3>
             <span>full interview</span>
             <iframe src="" frameborder="0"></iframe>
             <p>BARCELONA. <br> 2018</p>
         </div>
-        <div class="book-title">
-             <p><span>18</span> LA RADIO BARCELONA</p>
+        <div class="nine-book-title">
+             <p><span>18</span>flamantes #12</p>
         </div>
     </article>
 </section>
