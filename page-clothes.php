97a8f299c04c5f97d65d88f68768bdeb0f86d35e
natmacira <natimciraolo@gmail.com>
2023-09-12 21:17:31 +0200

    maquetado clothes page

--- page-clothes.php
+++ page-clothes.php
@@ -6,18 +6,35 @@ require_once 'header.php';
 
 // <!-- traeme el post type cloth  -->
 
-$query = new WP_Query( array( 'post_type' => 'cloth') );
+$query = new WP_Query(array('post_type' => 'cloth'));
 foreach ($query->posts as $cloth) {
-    echo $cloth->post_title; 
-    echo get_post_meta($cloth->ID, 'author', true); 
-    echo get_post_meta($cloth->ID, 'year', true); 
-    echo get_post_meta($cloth->ID, 'code', true); 
-    echo get_the_post_thumbnail( $cloth->ID, 'thumbnail', array( 'class' => 'cloth-img' ) );
-}
+
 ?>
-
-
+    <section>
+            <article>
+                <p>Author:</p>
+                <p> <?php echo esc_html(get_post_meta($cloth->ID, 'author', true)); ?> </p>
+            </article>
+            <article>
+                <p>Title:</p>
+                <p> <?php echo esc_html($cloth->post_title); ?> </p>
+            </article>
+            <article>
+                <p>Year:</p>
+                <p> <?php echo esc_html(get_post_meta($cloth->ID, 'year', true)); ?> </p>
+            </article>
+            <article>
+                <p>Code:</p>
+                <p> <?php echo esc_html(get_post_meta($cloth->ID, 'code', true)); ?>
+            </article>
+            <article>
+            <?php
+            echo get_the_post_thumbnail($cloth->ID, 'thumbnail', array('class' => 'cloth-img'));
+            ?>
+            </article>
+    </section>
 <?php
+}
 
 require_once 'footer.php';
 
