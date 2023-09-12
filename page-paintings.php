8dec517d3b051ee20413796b4e654b7dd0c5e39c
natmacira <natimciraolo@gmail.com>
2023-09-12 20:06:15 +0200

    added thumbnail print in page

--- page-paintings.php
+++ page-paintings.php
@@ -7,12 +7,13 @@ require_once 'header.php';
 // <!-- traeme el post type paintings  -->
 
 $query = new WP_Query( array( 'post_type' => 'painting') );
-var_dump($query->posts);
 foreach ($query->posts as $painting) {
     # code...
     echo $painting->post_title; 
     echo get_post_meta($painting->ID, 'year', true); 
     echo get_post_meta($painting->ID, 'technique', true); 
     echo get_post_meta($painting->ID, 'size', true); 
+    echo get_the_post_thumbnail( $painting->ID, 'thumbnail', array( 'class' => 'painting-img' ) );
+    // agregar img
 }
 ?>
