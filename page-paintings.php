67f3f4d0319f395fd2a623db49530c7dbe0bb408
natmacira <natimciraolo@gmail.com>
2023-09-12 21:26:43 +0200

    maquetado clothes paintings

--- page-paintings.php
+++ page-paintings.php
@@ -5,19 +5,35 @@ require_once 'header.php';
 
 
 // <!-- traeme el post type paintings  -->
-
-$query = new WP_Query( array( 'post_type' => 'painting') );
+$query = new WP_Query(array('post_type' => 'painting'));
 foreach ($query->posts as $painting) {
-    echo $painting->post_title; 
-    echo get_post_meta($painting->ID, 'year', true); 
-    echo get_post_meta($painting->ID, 'technique', true); 
-    echo get_post_meta($painting->ID, 'size', true); 
-    echo get_the_post_thumbnail( $painting->ID, 'thumbnail', array( 'class' => 'painting-img' ) );
-}
+
 ?>
-
-
+    <section>
+        <article>
+            <p>Name:</p>
+            <p> <?php echo esc_html($painting->post_title); ?> </p>
+        </article>
+        <article>
+            <p>Year:</p>
+            <p> <?php echo esc_html(get_post_meta($painting->ID, 'year', true)); ?> </p>
+        </article>
+        <article>
+            <p>Technique:</p>
+            <p> <?php echo esc_html(get_post_meta($painting->ID, 'technique', true)); ?> </p>
+        </article>
+        <article>
+            <p>Size:</p>
+            <p> <?php echo esc_html(get_post_meta($painting->ID, 'size', true)); ?>
+        </article>
+        <article>
+            <?php
+            echo get_the_post_thumbnail($painting->ID, 'thumbnail', array('class' => 'painting-img'));
+            ?>
+        </article>
+    </section>
 <?php
+}
 
 require_once 'footer.php';
 
