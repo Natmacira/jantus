bce58376270e2d36b75d10e7c7be6477ebf2f38b
natmacira <natimciraolo@gmail.com>
2023-11-23 11:45:40 +0100

    h1 header to home link

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <body class="<?php echo $body_class ?>">
 	<header>
 		<section class="titles">
-			<h1>Cutre Lux Jantus</h1>
+			<a href="<?php bloginfo('url'); ?>"><h1>Cutre Lux Jantus</h1></a>
 			<h2><?php echo esc_html(get_the_title()); ?></h2>
 		</section>
 		<section class="back-btn"><a href="javascript:history.back()"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/iconos/Atras.svg'); ?>" alt="Botón para volver atrás"></a></section>
