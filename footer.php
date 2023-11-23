a9be2d78d4fa76880330806a1204c14df5fa50b6
natmacira <natimciraolo@gmail.com>
2023-11-23 10:03:53 +0100

    add work open menu with correct links2

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
 		<li><a href="<?php echo esc_url(get_permalink(get_page_by_path('paintings'))); ?>">Paintings</a></li>
 		<li><a href="<?php echo esc_url(get_permalink(get_page_by_path('clothes'))); ?>">CLOTHES</a></li>
 		<li><a href="https://jantus.co/">Cybercafe</a></li>
-		<li><a href="<?php echo esc_url(get_permalink(get_page_by_path('clothes'))); ?>">VOXELS</a></li>
+		<li><a href="">VOXELS</a></li>
 	</ul>
 </footer>
 <!-- <?php wp_footer(); ?> -->
