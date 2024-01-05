d8a393bf06fdc1aace81b5f6d9cd56b89d77588d
natmacira <natimciraolo@gmail.com>
2024-01-04 21:05:55 -0300

    updatings first two books

--- page-publications.php
+++ page-publications.php
@@ -8,21 +8,40 @@ require_once 'header.php';
 
 <section class="library">
     <!-- <div> -->
-        <img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/publications/libro_publications.png'); ?>" alt="Book Publications" class="book-publications">
+    <img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/publications/libro_publications.png'); ?>" alt="Book Publications" class="book-publications">
     <!-- </div> -->
     <article class="library-inner-element">
         <div class="book-content one-book-content">
             <div class="titles-inner-container">
-                <h3>BONART</h3>
-                <span>full interview</span>
+                <h3>Bonart (Cataluña)</h3>
             </div>
-            <div class="iframe-container">
-                <iframe src="" frameborder="0"></iframe>
+            <div class="info-book-inner-container">
+                <p>Web: <br>
+                    <a href="https://www.bonart.cat/ca"> https://www.bonart.cat/ca</a>
+                </p>
+                <img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/publications/1_Bonart_Jantus.png'); ?>" alt="Portada del sitio web donde se presenta la obra de arte." class="portada">
+
+                <p>Bonart 1 Jantus y Galería Cor: <br>
+                    <a href="https://bit.ly/Bonart_Jantus_GaleríaCor"> https://bit.ly/Bonart_Jantus_GaleríaCor</a>
+                </p>
+                <img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/publications/2_Bonart_Jantus.png'); ?>" alt="Portada del sitio web donde se presenta la obra de arte." class="portada">
+                <p> Bonart 2 Jantus y Rose Madone:
+                    <br>
+                    <a href="https://bit.ly/Bonart_JantusRoseMadone"> https://bit.ly/Bonart_JantusRoseMadone </a>
+                </p>
+                <img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/publications/3_Bonart_Jantus.png'); ?>" alt="Portada del sitio web donde se presenta la obra de arte." class="portada">
+                <p> Bonart 3:
+                    <br>
+                    <a href="https://bit.ly/Bonart_Jantus_Fundació_Claret"> https://bit.ly/Bonart_Jantus_Fundació_Claret </a>
+                </p>
+                <p> Bonart 4:
+                    <br>
+                    <a href="https://bit.ly/Bonart_Jantus_Amuleto"> https://bit.ly/Bonart_Jantus_Amuleto </a>
+                </p>
             </div>
-            <p>test<br> 2018</p>
         </div>
         <div class="book-title one-book-title">
-            <p><span>18</span> BONART</p>
+            <p><span>18</span> Bonart (Cataluña)</p>
         </div>
     </article>
 
@@ -30,16 +49,23 @@ require_once 'header.php';
     <article class="library-inner-element">
         <div class="book-content two-book-content">
             <div class="titles-inner-container">
-                <h3>contemporary art curator</h3>
-                <span>full interview</span>
+                <h3>Flamantes #12</h3>
             </div>
-            <div class="iframe-container">
-                <iframe src="" frameborder="0"></iframe>
+            <div class="info-book-inner-container">
+                <p>Web: <br>
+                    <a href="https://bit.ly/Flamantes12_Jantus"> https://bit.ly/Flamantes12_Jantus</a>
+                </p>
+                <img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/publications/flamantes.jpg'); ?>" alt="Portada del sitio web donde se presenta la obra de arte." class="portada">
+
+                <p class="pdf-link"><a href="https://drive.google.com/file/d/13fmAJ_TtMZ35fl_z46pp2GvAm3MB28DM/view?usp=drive_link">PDF LINK</a></p>
+
+                <p>Video: <br>
+                    <a href="https://bit.ly/Video_Flamantes12_Lectura"> https://bit.ly/Video_Flamantes12_Lectura</a>
+                </p>
             </div>
-            <p>test<br> 2018</p>
         </div>
         <div class="book-title two-book-title">
-            <p><span>22</span> contemporary art curator</p>
+            <p><span>18</span> Flamantes #12</p>
         </div>
     </article>
 
