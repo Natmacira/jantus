af99dca4c309ee8d93de16a6470692a7469cb8fa
natmacira <natimciraolo@gmail.com>
2024-03-21 16:21:52 -0300

    re adapt footer in one block only cleaned code

--- footer.php
+++ footer.php
@@ -25,15 +25,7 @@
 			<li><a href="https://www.voxels.com/spaces/f480639a-c4b2-4130-ba2b-8b2671303c42/play">VOXELS</a></li>
 		</ul>
 	</div>
-	<div class="media-div">
-		<ul class="ul-mail">
-		</ul>
-		<!-- <ul class="ul-social">
-			<li><a href="https://opensea.io/JANTUS" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-opensee.svg'); ?>" alt="Link to opensee"></a></li>
-			<li><a href="https://www.youtube.com/channel/UCJ9e4sO1nlxais79BWPRJ7g" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-youtube.svg'); ?>" alt="Link to youtube"></a></li>
-			<li><a href="https://twitter.com/jantus_art" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-twiter.svg'); ?>" alt="Link to twitter" class="logo-x"></a></li>
-		</ul> -->
-	</div>
+	
 </footer>
 <!-- <?php wp_footer(); ?> -->
 </body>
