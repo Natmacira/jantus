e30ee4db035665556f9365c2da615c303a4b53e3
natmacira <natimciraolo@gmail.com>
2024-05-02 15:54:44 -0300

    re added social media ul

--- footer.php
+++ footer.php
@@ -24,6 +24,13 @@
 			<li><a href="<?php echo esc_url(get_permalink(get_page_by_path('cybercafe'))); ?>">Cybercafe</a></li>
 			<li><a href="https://www.voxels.com/spaces/f480639a-c4b2-4130-ba2b-8b2671303c42/play">VOXELS</a></li>
 		</ul>
+		<ul class="ul-social">
+			<li><a href="https://opensea.io/JANTUS" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-opensee.svg'); ?>" alt="Link to opensee"></a></li>
+			<li><a href="https://www.youtube.com/channel/UCJ9e4sO1nlxais79BWPRJ7g" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-youtube.svg'); ?>" alt="Link to youtube"></a></li>
+			<li><a href="https://twitter.com/jantus_" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-twiter.svg'); ?>" alt="Link to twitter" class="logo-x"></a></li>
+			<li><a href="hhttps://www.instagram.com/cutreluxorjantus?igsh=MTRzZmtoZHl3MW5idQ==" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-ig.png'); ?>" alt="Link to instagram" class="logo-x"></a></li>
+			<li class="zora"><a href="https://zora.co/@marcosgcutre" target="_blank"><img src="<?php echo esc_attr(get_stylesheet_directory_uri() . '/img/home/item-zora.png'); ?>" alt="Link to zora" class="logo-x"></a></li>
+		</ul>
 	</div>
 	
 </footer>
